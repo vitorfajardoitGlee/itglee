719fa30283044d525a62c18ebfbf4dfde0a13ba0
vitorfajardoitGlee <156077045+vitorfajardoitGlee@users.noreply.github.com>
2024-01-10 11:11:48 +0000

    Add files via upload

---
+++ itglee_new-main/itglee_new-main/itGlee/Models/Job.cs
@@ -0,0 +1,25 @@
+﻿namespace itGlee.Models
+{
+    public class Job
+    {
+        public string id;
+        public string position;
+        public string jobpositiondescription;
+        public JobDetails job;
+        public string imgurl;
+    }
+
+    public class JobDetails
+    {
+        public List<Section> sections;
+        public List<string> contractterms;
+        public List<string> tags;
+    }
+
+    public class Section
+    {
+        public string header;
+        public string text;
+        public List<string> bullets;
+    }
+}
